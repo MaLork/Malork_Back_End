3f5d5eca133d31038de6a7ab6bc8c228040ba9ea
palmcm <palmermonkeyboy@gmail.com>
2020-10-26 01:29:20 +0700

    Add getAllPosts

--- functions/index.js
+++ functions/index.js
@@ -2,5 +2,7 @@ const functions = require('firebase-functions');
 
 
 const post = require("./post")
+const posts = require("./posts")
 
 exports.post = functions.region("asia-east2").https.onRequest(post)
+exports.posts = functions.region("asia-east2").https.onRequest(posts)
