7e9e1a0e7898a74c06a38cd798402e5df16edec6
palmcm <palmermonkeyboy@gmail.com>
2020-10-28 23:38:34 +0700

    Add postComment

--- functions/comment/index.js
+++ functions/comment/index.js
@@ -3,9 +3,11 @@ const router = express.Router()
 
 const getCommentById = require("./getCommentById")
 const putComment = require("./putComment");
+const postComment = require("./postComment");
 
 router.get("/:id", getCommentById)
 router.put("/pick/:id", putComment);
+router.post("/:id", postComment);
 
 const app = express()
 const cors = require("cors")
