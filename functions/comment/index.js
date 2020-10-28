1bff935b692a30716cd792bc41e08852dd088bcf
CaQtiml <lsivakorn@gmail.com>
2020-10-28 22:04:36 +0700

    update PUT comment

--- functions/comment/index.js
+++ functions/comment/index.js
@@ -2,8 +2,10 @@ const express = require("express")
 const router = express.Router()
 
 const getCommentById = require("./getCommentById")
+const putComment = require("./putComment");
 
 router.get("/:id", getCommentById)
+router.put("/pick/:id", putComment);
 
 const app = express()
 const cors = require("cors")
