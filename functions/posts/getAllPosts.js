cf2342c7000b24b560b8706a137d62a51406de91
palmcm <palmermonkeyboy@gmail.com>
2020-10-30 16:58:46 +0700

    Add slice content

--- functions/posts/getAllPosts.js
+++ functions/posts/getAllPosts.js
@@ -10,11 +10,13 @@ module.exports = async (req,res) => {
             const {topic, user} = temp;
             const time = temp.time.toDate();
             const havePickedAnswer = temp.pick!==0;
+            const content = temp.content.slice(0,50);
             data.push({
                 topic,
                 user,
                 time,
                 havePickedAnswer,
+                content,
                 id: doc.id,
             })
           })
