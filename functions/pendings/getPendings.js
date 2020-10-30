e13b1eef3f99bc19a47e9169ef6f5b4548a17bfd
palmcm <palmermonkeyboy@gmail.com>
2020-10-31 01:10:57 +0700

    Sort post

--- functions/pendings/getPendings.js
+++ functions/pendings/getPendings.js
@@ -5,7 +5,7 @@ module.exports = async (req,res) => {
     try {
         data = [];
         //let data = req.body;
-        const docRef = db.collection("pending");
+        const docRef = db.collection("pending").orderBy("time", "desc");
         const snapshot = await docRef.get();
         snapshot.forEach(doc => {
             const temp = doc.data();
